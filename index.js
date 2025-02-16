a4c066d91796d784d68720499ab01de0cf6a694a
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-02-16 18:35:03 +0530

    Completed the main components and updated the commands

--- index.js
+++ index.js
@@ -26,3 +26,56 @@ player.on("connectionError 501" , (queue,error) =>{
 
 });
 
+player.on("trackStart", (queue, track) => {
+    queue.metadata.send(`🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+});
+
+player.on("trackAdd", (queue, track) => {
+    queue.metadata.send(`🎶 | Track **${track.title}** queued! This one is good OwO`);
+});
+
+player.on("botDisconnect", (queue) => {
+    queue.metadata.send("❌ | I was manually disconnected from the voice channel, clearing queue!");
+});
+
+player.on("channelEmpty", (queue) => {
+    queue.metadata.send("❌ | Nobody is in the voice channel, leaving...");
+});
+
+player.on("queueEnd", (queue) => {
+    queue.metadata.send("✅ | Queue finished!");
+});
+
+client.on("messageCreate" , async (message) {
+    if(message.author.bot || !message.guild) return;
+    if(!client.application?.owner) await client.application?.fetch();
+
+    if(message.content === "!deploy" && message.author.id === client.application.owner?.id) {
+        await message.guild.commands.set([
+            {
+            name: "play",
+            description: "Plays a Song from YT or ...",
+            Option:[{
+                name:"query",
+                type: "STRING",
+                description: "The song you want to play :)",
+                required:true
+            }]
+        },
+        
+        {
+            name:"skip",
+            description: "Skip to the new song"
+        },
+        
+        {
+            name:"stop",
+            description: "Stop the player"
+        },
+    ]);
+
+    await message.reply("Deployed!");
+
+    }
+});
+
