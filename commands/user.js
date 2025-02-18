050dbc5ce9d0018b5ddee6e167952a662f72d119
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-02-18 18:44:51 +0530

    Bot is online but commands doesn't work

---
+++ commands/user.js
@@ -0,0 +1,14 @@
+const {SlashCommandBuilder} = require("discord.js");
+const { execute } = require("./ping");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Provides infomation about the user.'),
+    async execute(interaction){
+        await interaction.reply(`This command was run by ${interaction.user.username}, Who joined on ${interaction.member.joinedAt}.`);
+
+    },
+
+};
+
